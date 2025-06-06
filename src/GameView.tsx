b54c0568c6ff3442de6be2f6d8518bb20f52b338
Kai Hay <kai@Kais-MacBook-Air.local>
2025-06-06 17:19:32 -0400

    dockerfile and changing urls

--- src/GameView.tsx
+++ src/GameView.tsx
@@ -26,7 +26,7 @@ export function GameView() {
   }, [initialGame])
 
   useEffect(() => {
-    const socket = io("http://localhost:3000")
+    const socket = io(process.env.DB_SERVER_URL)
     socket.on("connect", () => {
       socket.emit("join-game", game.id)
 
