ce01e9ed88a2a57fca3b49b67405b145def53e46
Kai Hay <kai@Kais-MacBook-Air.local>
2025-06-06 19:29:23 -0400

    add vercel stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react(), tailwindcss()],
-})
+});
